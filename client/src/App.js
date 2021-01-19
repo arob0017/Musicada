f47c0147ad9ba60f92fe40ca7592b879083dbe9e
Amelia Roberts <arob0017@student.monash.edu>
2021-01-19 20:48:23 +1100

    moving files around and debugged console errors

--- client/src/App.js
+++ client/src/App.js
@@ -3,40 +3,28 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 
-import NavBar from "./components/nav";
-import Landing from "./components/landing";
-import Register from "./components/authentication/register";
-import Login from "./components/authentication/login";
+import Navbar from "./components/nav";
+import Landing from "./pages/landing";
+import Register from "./pages/register";
+import Login from "./pages/login";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <div>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </div>
-      </Router>
-    );
-  }
-}
-
-export default App;
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
+          <Switch>
+            <Route>
+              <Login />
+            </Route>
+            <Route>
+              <Register />
+            </Route>
+            <Route>
+              <Landing />
+            </Route>
+          </Switch>
         </div>
       </Router>
     );
--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Landing() {
     return (
-        <container>
+        <>
             <div>
                 <h1>Hello World, landing page here</h1>
             </div>
-        </container>
+        </>
     );
 }
 
