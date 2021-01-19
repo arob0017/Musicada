c702ca79b0988d39500c4c9ba1fc03345b7d8099
Amelia Roberts <arob0017@student.monash.edu>
2021-01-19 21:26:28 +1100

    debugged errors, login page showing up

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import ButtonCmp from "../components/button";
 function Landing() {
     return (
         <>
+            <ButtonCmp>Landing Button</ButtonCmp>
             <div>
                 <h1>Hello World, landing page here</h1>
             </div>
